6eb32e1526ec21f14d51bd6c04be3097a5bf4c0b
Willis Plummer <willisplummer@gmail.com>
2018-11-02 15:29:42 -0400

    Updates

---
+++ precache-manifest.48521ed121de76ee187109e38923084e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92f717a78b8d25a07556",
+    "url": "/clark-styles/static/js/runtime~main.92f717a7.js"
+  },
+  {
+    "revision": "9ab858840898dfda8e9b",
+    "url": "/clark-styles/static/js/main.9ab85884.chunk.js"
+  },
+  {
+    "revision": "769856c50a997efd6431",
+    "url": "/clark-styles/static/js/1.769856c5.chunk.js"
+  },
+  {
+    "revision": "9ab858840898dfda8e9b",
+    "url": "/clark-styles/static/css/main.7a65263e.chunk.css"
+  },
+  {
+    "revision": "340d553b8c9a752815858324b6856e1e",
+    "url": "/clark-styles/index.html"
+  }
+];
