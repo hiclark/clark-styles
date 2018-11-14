c3d7a4caefd686c19d362f0a7bc217397eb5acf6
Jessica <jessica@hiclark.com>
2018-11-14 11:34:38 -0500

    Update Grid

--- src/flex-grid/grid/index.js
+++ src/flex-grid/grid/index.js
@@ -3,18 +3,24 @@
 import { type ComponentType } from 'react';
 
 import styled from 'styled-components';
+import GUTTER_SPACING from '../const';
 import SPACING from '../../constants/spacing';
 import MAX_WIDTH from '../../constants/max-width';
+import media from '../../constants/media-queries';
 
 type GridPropsType = {
   margin?: string,
 };
 
 export const Grid: ComponentType<GridPropsType> = styled.div`
-  padding: 0 ${SPACING.S_4};
+  padding: 0 ${GUTTER_SPACING};
   ${MAX_WIDTH};
   margin: ${({ margin }) => margin || '0 auto'};
   width: 100%;
+
+  ${media.small`
+    padding: 0 ${SPACING.S_4};
+  `};
 `;
 
 export default Grid;
