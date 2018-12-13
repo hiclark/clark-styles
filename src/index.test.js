59af7cb15600281adefa9beb3116d49a82140589
Willis Plummer <willisplummer@gmail.com>
2018-12-13 14:07:01 -0500

    update comment

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,8 @@
 // @flow
 
-// NOTE: this is just a placeholder so
-// circle passes until we add more tests.
+// TODO: this is just a placeholder so
+// circle passes until we add real tests.
+// Once we have real tests, we should delete.
 // - willis 12.13.18
 describe('Placeholder', () => {
   it('passes', () => {
