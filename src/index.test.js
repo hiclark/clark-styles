23a7358555a1a13ffd506ab124d9d3160fba34ba
willis plummer <willisplummer@gmail.com>
2018-12-14 11:31:34 -0500

    Passing Circle Builds (#12)
    ### Status
    **READY**
    
    ### Description
    **FIX**
    
    okay circle is passing now

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+// @flow
+
+// TODO: this is just a placeholder so
+// circle passes until we add real tests.
+// Once we have real tests, we should delete.
+// - willis 12.13.18
+describe('Placeholder', () => {
+  it('passes', () => {
+    expect(true).toBeTruthy();
+  });
+});
