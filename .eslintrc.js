a4fd102ffde8aafde00ed6f99b089079b3948f84
Chris D'Ascoli <chris@hiclark.com>
2019-07-25 14:52:35 -0400

    eslint to ignore peer dependenxy lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,31 +7,29 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    jest: true
+    jest: true,
   },
   globals: {
     mount: false,
     render: false,
-    shallow: false
+    shallow: false,
   },
   settings: {
     'import/resolver': {
       webpack: {
-        config: './config/webpack.config.base.js'
-      }
-    }
+        config: './config/webpack.config.base.js',
+      },
+    },
   },
   rules: {
-    "flowtype/require-valid-file-annotation": [
+    'flowtype/require-valid-file-annotation': [
       2,
-      "always", {
-        "annotationStyle": "line"
-      }
+      'always',
+      {
+        annotationStyle: 'line',
+      },
     ],
-    "flowtype/type-id-match": [
-      2,
-      "^([A-Z][a-z0-9]*)+Type$"
-    ],
+    'flowtype/type-id-match': [2, '^([A-Z][a-z0-9]*)+Type$'],
     'no-unused-vars': 2,
     'import/no-anonymous-default-export': 2,
     'prefer-object-spread/prefer-object-spread': 2,
@@ -39,21 +37,28 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'react/prop-types': 0,
     'react/require-default-props': 0,
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: true
-    }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
     'import/no-named-as-default-member': 0,
-    'jsx-a11y/anchor-is-valid': ['error', {
-      components: ['Link'],
-      specialLink: ['to', 'hrefLeft', 'hrefRight'],
-      aspects: ['noHref', 'invalidHref', 'preferButton']
-    }],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to', 'hrefLeft', 'hrefRight'],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+      },
+    ],
     'prettier/prettier': [
       'error',
       {
         singleQuote: true,
-        trailingComma: 'all'
-      }
-    ]
-  }
-}
+        trailingComma: 'all',
+      },
+    ],
+    'import/no-unresolved': ['error', { ignore: ['^react-router-dom$'] }],
+  },
+};
